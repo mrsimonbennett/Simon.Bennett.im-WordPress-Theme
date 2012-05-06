14735c21733401b28e5da42272837deaa0f4bc90
Simon Bennett <simon@bennett.im>
2012-05-06 13:51:31 +0000

    Add Piwik Tracking to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
 </div>
+<!-- Piwik --> 
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://traffic.vpdrive.com/" : "http://traffic.vpdrive.com/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://traffic.vpdrive.com/piwik.php?idsite=3" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
 </body>
 </html>
