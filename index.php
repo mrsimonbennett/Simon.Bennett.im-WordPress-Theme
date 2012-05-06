31bb618c7ec202213c8f58a4565136edd2940a1f
Simon Bennett <simon@bennett.im>
2012-05-06 11:42:25 +0000

    Added hr to blog and cleared up files

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
     <div class="post">
       <h3><a href="<?php the_permalink(); ?>  " ><?php the_title(); ?></a></h3>
+     <hr />
       <?php the_content(); ?>  
     </div>
     <?php endwhile; endif; ?>  
