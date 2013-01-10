3633e03e13b3ff688e902c77dc462c1c38c319ef
Simon Bennett <simon@bennett.im>
2013-01-10 11:22:28 +0000

    Added buttons to caresel on home page (recommended by  callumtaylor.net)

--- homepage.php
+++ homepage.php
@@ -43,6 +43,8 @@ Template Name: homepage
           </div>
         </div>
       </div>
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
      </div>
   </div>
   <div id="findme">
